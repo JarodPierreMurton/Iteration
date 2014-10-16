c4e3777fe1c4ea1f62d59a650da7303695946c50
Jarod Pierre Murton <45271@longroad.ac.uk>
2014-10-16 11:29:33 +0100

    Completed task 1 of iteration exercises

---
+++ Code imporving.py
@@ -0,0 +1,22 @@
+#Jarod Pierre Murton
+# Improving a peice of code
+#16/10/14
+
+total = 0
+
+for number in range(8):
+    number = int(input("Enter a number: "))
+    total = total + number
+print(total)
+    
+num1 = int(input('Enter number 1 : ' ))
+num2 = int(input('Enter number 2 : ' ))
+num3 = int(input('Enter number 3 : ' ))
+num4 = int(input('Enter number 4 : ' ))
+num5 = int(input('Enter number 5 : ' ))
+num6 = int(input('Enter number 6 : ' ))
+num7 = int(input('Enter number 7 : ' ))
+num8 = int(input('Enter number 8 : ' ))
+
+total = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8
+print('The total is {0}'.format(total))
